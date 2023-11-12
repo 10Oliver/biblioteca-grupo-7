3a42b38ad9c750a7696ab316b6fbe6ed7229b334
fjose4210 <132023217+fjose4210@users.noreply.github.com>
2023-11-12 16:55:59 -0600

    Vista Usuario no completa

--- src/vistas/usuario.java
+++ src/vistas/usuario.java
@@ -5,6 +5,8 @@
  */
 package vistas;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Oliver
@@ -28,6 +30,10 @@ public class usuario extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
+        Agregar = new javax.swing.JButton();
+        Restablecer = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setMinimumSize(new java.awt.Dimension(1030, 640));
@@ -35,26 +41,179 @@ public class usuario extends javax.swing.JPanel {
 
         jLabel1.setText("Usuarios");
 
+        Agregar.setText("Agregar");
+        Agregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AgregarActionPerformed(evt);
+            }
+        });
+
+        Restablecer.setText("Restablecer");
+        Restablecer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RestablecerActionPerformed(evt);
+            }
+        });
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "#", "Nombre de usuario", "Correo", "Fecha de nacimiento", "Telefono", "Rol"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Short.class, java.lang.Object.class, java.lang.Object.class, java.lang.String.class, java.lang.String.class, java.lang.Object.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(jTable1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(363, 363, 363)
-                .addComponent(jLabel1)
-                .addContainerGap(618, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(372, 372, 372)
+                        .addComponent(Agregar)
+                        .addGap(141, 141, 141)
+                        .addComponent(Restablecer))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(495, 495, 495)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(144, 144, 144)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 737, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(149, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(123, 123, 123)
+                .addGap(40, 40, 40)
                 .addComponent(jLabel1)
-                .addContainerGap(501, Short.MAX_VALUE))
+                .addGap(31, 31, 31)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Agregar)
+                    .addComponent(Restablecer))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(424, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void AgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AgregarActionPerformed
+        // TODO add your handling code here:
+        String nombreUsuario = JOptionPane.showInputDialog(this, "Ingrese nombre de usuario:");
+        String correo = JOptionPane.showInputDialog(this, "Ingrese correo:");
+        String fechaNacimiento = JOptionPane.showInputDialog(this, "Ingrese fecha de nacimiento (YYYY-MM-DD):");
+        String telefono = JOptionPane.showInputDialog(this, "Ingrese teléfono:");
+        String rol = JOptionPane.showInputDialog(this, "Ingrese rol:");
+        String contrasenia = JOptionPane.showInputDialog(this, "Ingrese contraseña:");
+        String confirmarContrasenia = JOptionPane.showInputDialog(this, "Confirme la contraseña:");
 
+        // Verificar si las contraseñas coinciden
+        if (contrasenia.equals(confirmarContrasenia)) {
+            // Agregar el nuevo usuario a la tabla
+            agregarUsuario(nombreUsuario, correo, fechaNacimiento, telefono, rol);
+        } else {
+            JOptionPane.showMessageDialog(this, "Las contraseñas no coinciden. Inténtelo de nuevo.");
+        }
+                                        
+    }//GEN-LAST:event_AgregarActionPerformed
+
+    private void RestablecerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RestablecerActionPerformed
+      // Pide al usuario que ingrese su nombre de usuario
+    String nombreUsuario = JOptionPane.showInputDialog(this, "Ingrese su nombre de usuario:");
+
+    // Verifica si el nombre de usuario existe en la tabla
+    if (buscarUsuario(nombreUsuario)) {
+        // Si existe, permite al usuario restablecer la contraseña
+        restablecerContrasenia(nombreUsuario);
+    } else {
+        JOptionPane.showMessageDialog(this, "El nombre de usuario no existe. Inténtelo de nuevo.");
+    }
+}  
+// Método para restablecer la contraseña de un usuario
+private void restablecerContrasenia(String nombreUsuario) {
+    // Obtener el modelo de la tabla
+    javax.swing.table.DefaultTableModel model = (javax.swing.table.DefaultTableModel) jTable1.getModel();
+
+    // Buscar la fila correspondiente al usuario
+    int filaUsuario = -1;
+    for (int i = 0; i < model.getRowCount(); i++) {
+        if (nombreUsuario.equals(model.getValueAt(i, 1))) {
+            filaUsuario = i;
+            break;
+        }
+    }
+
+    // Solicitar la nueva contraseña
+    String nuevaContrasenia = JOptionPane.showInputDialog(this, "Ingrese la nueva contraseña:");
+
+    // Verificar que se haya ingresado una nueva contraseña
+    if (nuevaContrasenia != null) {
+        // Confirmar la nueva contraseña
+        String confirmarNuevaContrasenia = JOptionPane.showInputDialog(this, "Confirme la nueva contraseña:");
+
+        // Verificar que la contraseña confirmada coincida
+        if (nuevaContrasenia.equals(confirmarNuevaContrasenia)) {
+            // Actualizar la contraseña en la tabla (y posiblemente en tu sistema)
+            model.setValueAt(nuevaContrasenia, filaUsuario, 5); // Suponiendo que la contraseña está en la columna 5
+            JOptionPane.showMessageDialog(this, "La contraseña ha sido restablecida exitosamente.");
+        } else {
+            JOptionPane.showMessageDialog(this, "Las contraseñas no coinciden. Inténtelo de nuevo.");
+        }
+    } else {
+        JOptionPane.showMessageDialog(this, "No se ha ingresado una nueva contraseña.");
+    }
+    }//GEN-LAST:event_RestablecerActionPerformed
+
+
+        
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Agregar;
+    private javax.swing.JButton Restablecer;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
+private int numeroDeFila = 1;
+
+// Método para agregar un nuevo usuario a la tabla
+private void agregarUsuario(String nombreUsuario, String correo, String fechaNacimiento, String telefono, String rol) {
+    // Obtener el modelo de la tabla
+    javax.swing.table.DefaultTableModel model = (javax.swing.table.DefaultTableModel) jTable1.getModel();
+
+    // Agregar una nueva fila con los datos del nuevo usuario
+    model.addRow(new Object[]{
+        numeroDeFila,
+        nombreUsuario,
+        correo,
+        fechaNacimiento,
+        telefono,
+        rol
+    });
+
+    // Incrementar el número de fila para la siguiente inserción
+    numeroDeFila++;
+ }
+
+    private boolean buscarUsuario(String nombreUsuario) {
+     javax.swing.table.DefaultTableModel model = (javax.swing.table.DefaultTableModel) jTable1.getModel();
+
+    // Buscar el nombre de usuario en la columna correspondiente
+    for (int i = 0; i < model.getRowCount(); i++) {
+        if (nombreUsuario.equals(model.getValueAt(i, 1))) {
+            return true; // Usuario encontrado
+        }
+    }
+
+    return false; // Usuario no encontrado
+   }
 }
